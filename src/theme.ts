e974d5d136b9dc5d9042f0ab83ffd80e507974ba
Steve <kgogo9@gmail.com>
2021-12-07 00:28:18 +0900

    styled components theme change

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+};
+
+const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+};
+
+export { lightTheme, darkTheme };
