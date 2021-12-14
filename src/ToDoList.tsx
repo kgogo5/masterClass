02f6a186347bb0e6a5bcc3d1156be65d690dcb99
Steve <kgogo9@gmail.com>
2021-12-14 22:27:49 +0900

    To do list 셋팅

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  toDo: string;
+}
 function ToDoList() {
-  const { register, handleSubmit } = useForm();
-  const onValid = (data: any) => {
-    console.log(data);
+  const { register, handleSubmit, setValue } = useForm();
+  const handleValid = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
+      <form onSubmit={handleSubmit(handleValid)}>
         <input {...register("toDo")} placeholder="Write a to do" />
         <button>Add</button>
       </form>
