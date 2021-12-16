aae5ac33c015c670c96bce3f7859a46161a38f09
Steve <kgogo9@gmail.com>
2021-12-16 23:10:42 +0900

    애니메이션 framer-motion 초기 세팅

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#f5f6fa",
-  accentColor: "#8866ee",
-};
-
-export { darkTheme };
+export const darkTheme: DefaultTheme = {};
